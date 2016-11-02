0878e8c27070bace6d160ae919a5a82d51a33faf
stephen <stejstin@ut.utm.edu>
2016-11-02 13:12:49 -0500

    New main, since it's been modified with a sum function

--- main.cpp
+++ main.cpp
@@ -7,8 +7,26 @@
 #include <iostream>
 using namespace std;
 
+
+int sum(int n);//prototype for the sum method
+
+
 int main(){
-
+  int n;
+  
   cout << "HELLO WORLD!" << endl;
+  cout << "Now Please enter a number ";
+  cin >> n;
 
+  cout << "The sum is: "  << sum(n) << endl;
+
+}
+
+int sum(int n){
+  int sum1 = 0;
+  for(int i = 0; i <= n; i++){
+    sum1 += i;
+  }
+
+  return sum1;
 }
